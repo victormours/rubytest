951662205cdea161284d201481842ee487521fed
Victor Mours <victor.mours@ensimag.imag.fr>
2013-04-22 20:56:56 -0500

    done problems 5 and 6, started 7

--- problem1.rb
+++ problem1.rb
@@ -1,7 +1,7 @@
-class Problem1
+module Problem1
     #This method takes some object that responds to the each method, like an array, and returns a string representation of the values
     #similar to what irb would display.
-    def something_unusual(values)
+    def Problem1.something_unusual(values)
         temp = "["
         values.each do |value|
             temp += "#{value.to_s}, "
--- spec/problem1_spec.rb
+++ spec/problem1_spec.rb
@@ -1,18 +1,17 @@
 require './problem1.rb'
 
 describe Problem1 do
-    before do @p = Problem1.new end
 
     it "should return [] for an empty array" do
-        @p.something_unusual([]).should == "[]"
+        Problem1.something_unusual([]).should == "[]"
     end
 
     it "should work for a single value" do
-        @p.something_unusual(["a"]).should == "[a]"
+        Problem1.something_unusual(["a"]).should == "[a]"
     end
 
     it "should work for a couple of values" do
-        @p.something_unusual([1, 2, 3]).should == "[1, 2, 3]"
+        Problem1.something_unusual([1, 2, 3]).should == "[1, 2, 3]"
     end
 
 end
