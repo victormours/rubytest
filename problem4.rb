634b37d95c861ee0bbd159c6412d94f65f2ac923
Victor Mours <victor.mours@ensimag.imag.fr>
2013-04-30 14:01:54 -0500

    refactored problem4 to remove feature envy

--- problem4.rb
+++ problem4.rb
@@ -3,8 +3,7 @@
 #I'm assuming that we still want to be able to feed pets with some unconventional food
 #but that we want an easy and more elegant way of deciding which is the best type of food.
 
-#The heavy-handed way of implementing strategy would be with a FeedStrategy and three subclasses, maybe an abstract factory.
-#But let's keep it simple: giving each pet a favorite food is probably the easiest solution.
+#Giving the food argument a default value avoids feature envy
 
 class Pet
     def feed(food)
@@ -13,20 +12,20 @@ class Pet
 end
 
 class Cat < Pet
-    def favorite_food
-        :milk
+    def feed(food = :milk)
+        super
     end
 end
 
 class Dog < Pet
-    def favorite_food
-        :dogfood
+    def feed(food = :dogfood)
+        super
     end
 end
 
 class Cow < Pet
-    def favorite_food
-        :grass
+    def feed(food = :grass)
+        super
     end
 end
 
@@ -36,5 +35,5 @@ cow = Cow.new
 my_pets = [cat, dog, cow, ]
 
 my_pets.each do |pet|
-    pet.feed pet.favorite_food
+    pet.feed
 end
